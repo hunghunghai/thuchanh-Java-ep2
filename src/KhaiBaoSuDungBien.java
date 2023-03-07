6aaab055eb5157192429471c423d94a8c923f752
Nguyễn Phi Hùng <phuongpidz@gmail.com>
2023-03-07 10:33:27 +0700

    thực hành

---
+++ src/KhaiBaoSuDungBien.java
@@ -0,0 +1,14 @@
+public class KhaiBaoSuDungBien {
+    public static void main(String[] args) {
+        int i = 10; // khai báo biến i là kiểu  int có giá trị là 10
+        float f = 20.5f; //khai báo biến f là kiểu float có giá trị là 20.5f
+        boolean  b = true; //khai báo biến b là kiểu boolean có giá trị true
+        char c = 'a'; //khai báo biến c có kiểu char có giá trị là ký tự a
+        String s = "Hà nội"; //khai báo biến s có kiểu String có giá trị là chuỗi Hà Nội
+        System.out.println("i = " +i);
+        System.out.println("f = " +f);
+        System.out.println("b = " +b);
+        System.out.println("c = " +c);
+        System.out.println("s = " +s);
+    }
+}
